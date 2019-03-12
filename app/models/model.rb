5aa6222026d506952595fdde39d51f0f1c393c18
Mickey <micksheridan.24@gmail.com>
2019-03-12 08:53:02 -0400

    submit

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
-class Model
+class Dog
+  attr_accessor :name, :age
+  attr_reader :breed
+  @@all = []
 
+  def initialize(name, breed, age)
+    @name, @age, @breed = name, age, breed
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
